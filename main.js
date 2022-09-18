82a14b7d9eeec8e16740e59c8e727dd93c4a2ae3
Dhir-N <88658011+Dhir-N@users.noreply.github.com>
2022-09-18 22:26:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function weight(){
+    window.location = "weight.html";
+}
+
+function diet(){
+    window.location = "diet.html";
+}
+
+function exercise(){
+    window.location = "exercise.html";
+}
